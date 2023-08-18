902a92816914ca3e6b70406e18d5a8a26367cbd8
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-08-18 12:42:28 +0200

    modify some styling on the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import { Button } from "@mui/material";
 import Styles from "./Header.module.css";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
+import ApiIcon from "@mui/icons-material/Api";
 
 function Header() {
   return (
     <div className={Styles.header}>
       <div className={Styles.title}>
-        <h1>Routers and Context API</h1>
+        <h1>
+          <ApiIcon />
+          Routers and Context API
+        </h1>
       </div>
       <div className={Styles.modeBox}>
-        <h3>Change the mode: </h3>
+        <h3>
+          Change the <br /> mode:{" "}
+        </h3>
         <button className={Styles.mode}>
           <Brightness4Icon />
         </button>
